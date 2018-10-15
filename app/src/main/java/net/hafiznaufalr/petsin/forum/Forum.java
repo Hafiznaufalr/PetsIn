d3f6429d955dbf804a580a63e505d701a671ba0b
Hafiz Naufal Rahman <hafiz.nr5758@gmail.com>
2018-10-16 05:36:03 +0700

    add Forum

---
+++ app/src/main/java/net/hafiznaufalr/petsin/forum/Forum.java
@@ -0,0 +1,181 @@
+package net.hafiznaufalr.petsin.forum;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.scaledrone.lib.Listener;
+import com.scaledrone.lib.Member;
+import com.scaledrone.lib.Room;
+import com.scaledrone.lib.RoomListener;
+import com.scaledrone.lib.Scaledrone;
+
+import net.hafiznaufalr.petsin.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class Forum extends AppCompatActivity implements RoomListener {
+
+    // replace this with a real channelID from Scaledrone dashboard
+    private String channelID = "406saFMc6NX8whpc";
+    private String roomName = "observable-room";
+    private EditText editText;
+    private Scaledrone scaledrone;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
+
+        MemberData data = new MemberData(getRandomName(), getRandomColor());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Forum");
+
+        scaledrone = new Scaledrone(channelID, data);
+        scaledrone.connect(new Listener() {
+            @Override
+            public void onOpen() {
+                System.out.println("Scaledrone connection open");
+                scaledrone.subscribe(roomName, Forum.this);
+            }
+
+            @Override
+            public void onOpenFailure(Exception ex) {
+                System.err.println(ex);
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+                System.err.println(ex);
+            }
+
+            @Override
+            public void onClosed(String reason) {
+                System.err.println(reason);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            scaledrone.publish(roomName, message);
+            editText.getText().clear();
+        }
+    }
+
+    @Override
+    public void onOpen(Room room) {
+        System.out.println("Conneted to room");
+    }
+
+    @Override
+    public void onOpenFailure(Room room, Exception ex) {
+        System.err.println(ex);
+    }
+
+    @Override
+    public void onMessage(Room room, final JsonNode json, final Member member) {
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            final MemberData data = mapper.treeToValue(member.getClientData(), MemberData.class);
+            boolean belongsToCurrentUser = member.getId().equals(scaledrone.getClientID());
+            final Message message = new Message(json.asText(), data, belongsToCurrentUser);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    messageAdapter.add(message);
+                    messagesView.setSelection(messagesView.getCount() - 1);
+                }
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getRandomName() {
+        String[] adjs = {"user1", "user2","user3"};
+        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        return (
+                adjs[(int) Math.floor(Math.random() * adjs.length)] +
+                        "_" +
+                        nouns[(int) Math.floor(Math.random() * nouns.length)]
+        );
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
+    }
+}
+
+class MemberData {
+    private String name;
+    private String color;
+
+    public MemberData(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public MemberData() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberData{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/net/hafiznaufalr/petsin/fragments/InboxFragment.java
+++ app/src/main/java/net/hafiznaufalr/petsin/fragments/InboxFragment.java
@@ -1,16 +1,26 @@
 package net.hafiznaufalr.petsin.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import net.hafiznaufalr.petsin.R;
+import net.hafiznaufalr.petsin.events.Events;
+import net.hafiznaufalr.petsin.forum.Forum;
+import net.hafiznaufalr.petsin.lifestyle.LifeStyle;
+import net.hafiznaufalr.petsin.nearby.MapsActivity;
 
-public class InboxFragment extends Fragment {
+public class InboxFragment extends Fragment implements View.OnClickListener {
+
+    RelativeLayout r1,r2,r3,r4;
 
     public InboxFragment() {
         // Required empty public constructor
@@ -22,8 +32,41 @@ public class InboxFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inbox, container, false);
 
+        r1 = (RelativeLayout) view.findViewById(R.id.inb1);
+        r1.setOnClickListener((View.OnClickListener) this);
+        r2 = (RelativeLayout) view.findViewById(R.id.inb2);
+        r2.setOnClickListener((View.OnClickListener) this);
+        r3 = (RelativeLayout) view.findViewById(R.id.inb3);
+        r3.setOnClickListener((View.OnClickListener) this);
+        r4 = (RelativeLayout) view.findViewById(R.id.inb4);
+        r4.setOnClickListener((View.OnClickListener) this);
+
 
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.inb1:
+                Intent yIntent = new Intent(getActivity(), Forum.class);
+                startActivity(yIntent);
+                break;
+
+            case R.id.inb4:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "app3nity@gmail.com", null));
+
+                getActivity().startActivity(Intent.createChooser(emailIntent, null));
+                break;
+
+
+
+
+        }
+
+
+    }
 }
